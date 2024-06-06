1885e7443efe47ebfb95c32440e681746afabc42
CR7sewey <miguelmotacarvalho@gmail.com>
2024-06-06 14:49:23 +0100

    07-slider

---
+++ 07-slider/starter/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { longList } from "./data";
+import Carousel from "./Carousel";
+const App = () => {
+  return (
+    <main>
+      <Carousel />
+    </main>
+  );
+};
+export default App;
